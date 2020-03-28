48a7ea303dd3f9df6efc6663394114b3ae8a3c37
Matt Henry <matt@henrymatt.com>
2020-03-28 17:39:09 -0600

    implement login view controller

--- OnTheMap/ViewControllers/LoginViewController.swift
+++ OnTheMap/ViewControllers/LoginViewController.swift
@@ -9,22 +9,23 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField?
+    @IBOutlet weak var passwordField: UITextField?
+    @IBOutlet weak var loginButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func login(sender: UIButton?) {
+        print("Login")
     }
-    */
+    
+    @IBAction func signup(sender: UIButton?) {
+        if let link = URL(string: "https://auth.udacity.com/sign-up") {
+            UIApplication.shared.open(link)
+        }
+    }
 
 }
