eb3d7a5530b83e676175ed9236e7ce1ee222882b
Matt Henry <matt@henrymatt.com>
2020-03-28 15:50:44 -0600

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Matt Henry on 3/28/20.
+//  Copyright © 2020 Beardbird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
